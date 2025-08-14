02141f5270e985bc550a3254f5cfc8eb007424c7
caspheon <contact@caspheon.com>
2025-08-13 21:14:00 -0300

    database

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   experimental: {
     // Removendo appDir pois já é padrão no Next.js 14
   },
+  // Garantir que os aliases funcionem
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '.',
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
